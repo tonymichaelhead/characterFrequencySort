aadc51289709b2b139aeffd3240e1a9a6e4364c6
tonymichaelhead <tonymichaelhead@gmail.com>
2017-10-07 12:12:46 +0900

    Code cleanup and refactor

--- characterFrequency.js
+++ characterFrequency.js
@@ -5,32 +5,31 @@ O: array of arrays descending order of frequency then ascending order by charact
 
 function characterFrequency (string) {
     //make histogram
-      let hist = {};
-      string.split('').forEach(function(char) {
+    const hist = {};
+    string.split('').forEach(char => {
         if(hist[char]) {
-          hist[char] += 1;
+            hist[char] += 1;
         } else {
-          hist[char] = 1;
+            hist[char] = 1;
         }
-      });
+    });
       
-      let tuples = [];
-      for (var key in hist) {
+    const tuples = [];
+    for (var key in hist) {
         tuples.push([key, hist[key]]);
-      }
-    
-      tuples.sort((a,b) => {
-        if (a[1] === b[1]) {
-          if (a[0] > b[0]) {
-            return 1;
-          } else {
-            return -1;
-          }
-        } else {
-          return b[1] - a[1];
-        }
-      });
-    
-    return tuples;
     }
+    
+    tuples.sort((a,b) => {
+        if (a[1] === b[1]) {
+            if (a[0] > b[0]) {
+            return 1;
+            } else {
+            return -1;
+            }
+        } else {
+            return b[1] - a[1];
+        }
+    });
+    return tuples;
+}
     
