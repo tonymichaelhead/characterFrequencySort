25d420dd06943b11e75c05e112cf48c0f7e2d847
tonymichaelhead <tonymichaelhead@gmail.com>
2017-10-07 11:50:27 +0900

    Implement solution

---
+++ characterFrequency.js
@@ -0,0 +1,36 @@
+/*
+I: input string of characters
+O: array of arrays descending order of frequency then ascending order by character
+*/
+
+function characterFrequency (string) {
+    //make histogram
+      let hist = {};
+      string.split('').forEach(function(char) {
+        if(hist[char]) {
+          hist[char] += 1;
+        } else {
+          hist[char] = 1;
+        }
+      });
+      
+      let tuples = [];
+      for (var key in hist) {
+        tuples.push([key, hist[key]]);
+      }
+    
+      tuples.sort((a,b) => {
+        if (a[1] === b[1]) {
+          if (a[0] > b[0]) {
+            return 1;
+          } else {
+            return -1;
+          }
+        } else {
+          return b[1] - a[1];
+        }
+      });
+    
+    return tuples;
+    }
+    
